93c51db4559de802717ed9fa1223a8a39bcaaf9e
kato707gg <katoh707@gmail.com>
2024-04-08 23:31:56 -0600

    7

--- app_estudios_calidad/lib/Screens/startPage.dart
+++ app_estudios_calidad/lib/Screens/startPage.dart
@@ -1,29 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 class StartPage extends StatelessWidget {
   const StartPage({super.key});
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      resizeToAvoidBottomInset: false,
+    return const Scaffold(
+      backgroundColor: Color.fromARGB(255, 216, 216, 216),
       body: Stack(
-        children: [
-          Positioned(
-            top: 0,
-            left: 0,
-            right: 0,
-            bottom: MediaQuery.of(context).size.height / 1.7,
-            child: Background(),
-          ),
-          Positioned(
-            top: MediaQuery.of(context).size.height / 2.6,
-            left: 0,
-            right: 0,
-            bottom: 0,
-            child: Homebody(),
-          ),
-        ],
+        children: [Background(), Homebody()],
       ),
     );
   }
@@ -33,14 +17,7 @@ class Background extends StatelessWidget {
   const Background({super.key});
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        image: DecorationImage(
-          image: AssetImage('assets/Fondooo.jpeg'),
-          fit: BoxFit.cover,
-        ),
-      ),
-    );
+    return Container();
   }
 }
 
